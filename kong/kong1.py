d689c769bf59dbba4bf83f3f6cc4d72daeceabe6
xuandonglai <xuandonglai@tencent.com>
2023-08-03 22:48:04 +0800

    minor: 更新

--- kong/kong1.py
+++ kong/kong1.py
@@ -17,12 +17,12 @@ class BaseModel(models.Model):
 
 
 class BizConfigModel(BaseModel):
-    bk_product_id = models.IntegerField("CMDB产品ID", null=True, blank=True)
-    bk_product_name = models.CharField("CMDB产品名称", max_length=100, null=True, blank=True)
-    bk_biz_id = models.IntegerField("蓝鲸业务ID", unique=True)
-    bk_biz_name = models.CharField("蓝鲸业务名称", max_length=100)
-    secret_id = SymmetricTextField("secret_id", max_length=256, default="")  # 支持国密（对称加密）
-    secret_key = SymmetricTextField("secret_key", max_length=256, default="")  # 支持国密（对称加密）
+    product_id = models.IntegerField("产品ID", null=True, blank=True)
+    product_name = models.CharField("产品名称", max_length=100, null=True, blank=True)
+    biz_id = models.IntegerField("业务ID", unique=True)
+    biz_name = models.CharField("业务名称", max_length=100)
+    secret_id = SymmetricTextField("id", max_length=256, default="")
+    secret_key = SymmetricTextField("key", max_length=256, default="")
 
     objects = BulkUpdateOrCreateQuerySet.as_manager()
 
@@ -36,10 +36,10 @@ class BizConfigModel(BaseModel):
 
 
 class BizCLBListModel(BaseModel):
-    bk_biz_id = models.IntegerField("蓝鲸业务ID")
+    biz_id = models.IntegerField("业务ID")
     region = models.CharField("地域", max_length=100)
     handler_name = models.CharField("handler类型", max_length=100)
-    is_internal_api = models.BooleanField("是否启用内网API", null=True, blank=True)
+    is_internal_api = models.BooleanField("是否用内网API", null=True, blank=True)
     VpcId = models.CharField("虚拟私有云ID", max_length=256)
     LoadBalancerId = models.CharField("负载均衡实例ID", max_length=256)
     LoadBalancerVips = models.CharField("负载均衡实例VIP", max_length=256)
